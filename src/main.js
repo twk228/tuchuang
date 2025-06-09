fc2a46bc05bba18f0bd2b3c05dbbed5499fc4af4
twk <twk22856@163.com>
2025-06-09 14:07:58 +0800

    feat(src): 连接 Neon 数据库并执行 SQL 查询
    - 使用 Neon 库连接到 Azure Neon 数据库
    - 执行 SQL 查询以获取 classify_list 表中的所有记录
    - 打印查询结果到控制台
    - 注释掉之前的测试代码，保留潜在的 INSERT 和 DELETE 语句作为示例

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { neon } from '@netlify/neon';
-console.log('neon',neon);
-// const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
-// console.log('sql',sql);
-// const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
+const sql = neon('postgresql://kedouzhihuiDB_owner:npg_tHU2FgQCOIA5@ep-shy-sunset-a8hk884g-pooler.eastus2.azure.neon.tech/kedouzhihuiDB?sslmode=require'); // automatically uses env NETLIFY_DATABASE_URL
+// await sql`INSERT INTO public.classify_list (name, icon) VALUES ('React', '');`;
+// await sql`DELETE FROM public.classify_list WHERE NAME = 'React';`;
+const classifyList = await sql`SELECT * FROM classify_list`;
+console.log('classifyList',classifyList);
 
 createApp(App).mount('#app')
